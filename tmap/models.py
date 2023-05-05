5f47b7ccaa379d0f06cd5a0370cbcfc6c8c15364
YPYP333YPYP <ypyp333ypyp@gmail.com>
2023-05-05 19:53:36 +0900

    Tmap api - 센터, 권역, 차량, 배송지, 배차, 배차결과 model 생성

--- tmap/models.py
+++ tmap/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class GeoInformation(models.Model):
@@ -16,22 +17,46 @@ class CenterList(models.Model):
     geo = models.ForeignKey(GeoInformation, null=True, blank=True, on_delete=models.SET_NULL)
 
     def __str__(self):
-        return f'{self.geo.name}'
-
+        return f'{self.centerName}'
 
 
 class ZonList(models.Model):
-    pass
+    code = models.CharField(max_length=100, null=True)
+    name = models.CharField(max_length=100, null=True)
 
 
 class VehicleList(models.Model):
-    pass
+    vehicleId = models.CharField(max_length=100, null=True)
+    vehicleName = models.CharField(max_length=100, null=True)
+    weight = models.IntegerField(null=True)
+    vehicleType = models.CharField(max_length=100, default="2")
+    zoneCode = models.CharField(max_length=100, null=True)
+    inputYn = models.CharField(max_length=10, default="1")
+    skillPer = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(100)], null=True)
+    volume = models.IntegerField(null=True)
 
 
 class OrderList(models.Model):
-    pass
+    orderId = models.CharField(max_length=100, null=True)
+    orderName = models.CharField(max_length=100, null=True)
+    geo = models.ForeignKey(GeoInformation, null=True, blank=True, on_delete=models.CASCADE)
+    vehicleType = models.CharField(max_length=100, default="2")
+    serviceTime = models.IntegerField(null=True)
+    zoneCode = models.CharField(max_length=100, null=True)
+    deliveryWeight = models.CharField(max_length=100, null=True)
+    deliveryVolume = models.CharField(max_length=100, null=True)
+    updateDate = models.DateTimeField(null=True)
 
 
 class DispatchList(models.Model):
-    pass
+    orderList = models.ForeignKey(OrderList, blank=True, null=True, on_delete=models.CASCADE)
+    vehicleList = models.ForeignKey(VehicleList, blank=True, null=True, on_delete=models.CASCADE)
+    startTime = models.CharField(max_length=10, blank=False, null=True)
+    optionType = models.CharField(max_length=10, null=True)
+    mappingKey = models.CharField(max_length=100, null=True)
 
+
+class DispatchListResult(models.Model):
+    orderList = models.ForeignKey(OrderList, blank=False, on_delete=models.CASCADE)
+    vehicleList = models.ForeignKey(VehicleList, blank=False, on_delete=models.CASCADE)
+    routeList = models.CharField(max_length=1000)
